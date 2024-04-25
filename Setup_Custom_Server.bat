e2d1350e03e7a0cec5ce631addbb7ea9b5fdc73e
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-04-25 21:00:59 +0800

    Add new servers

--- Setup_Custom_Server.bat
+++ Setup_Custom_Server.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Downloading region info from aug.233466.xyz
+echo Downloading custom regions info from aug.233466.xyz
 curl -L --output "%APPDATA%/../LocalLow/Innersloth/Among Us/regionInfo.json" --url "https://aug.233466.xyz/regionInfo.json"
 echo Finished! Open your AmongUs to see the result!
 pause
