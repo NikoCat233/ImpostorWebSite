986b71093f9bbb4a4c331a89940dccfbbda43bfb
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-04-30 17:08:44 +0800

    Update Setup_Custom_Server.bat

--- Setup_Custom_Server.bat
+++ Setup_Custom_Server.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Downloading custom regions info from aug.233466.xyz
-curl -L --output "%APPDATA%/../LocalLow/Innersloth/Among Us/regionInfo.json" --url "https://aug.233466.xyz/regionInfo.json"
+curl -L --output "%APPDATA%/../LocalLow/Innersloth/Among Us/regionInfo.json" --url "https://aug.233466.xyz/regioninfo.json"
 echo Finished! Open your AmongUs to see the result!
 pause
