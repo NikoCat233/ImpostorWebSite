0f6fd057959b8b2abe9a0fb665717ae913985de7
Niko233 <139348239+NikoCat233@users.noreply.github.com>
2024-06-04 15:23:29 +0800

    Add files via upload

--- Setup_Custom_Server.bat
+++ Setup_Custom_Server.bat
@@ -1,5 +1,13 @@
 @echo off
-echo Downloading custom regions info from aug.233466.xyz
-curl -L --output "%APPDATA%/../LocalLow/Innersloth/Among Us/regionInfo.json" --url "https://aug.233466.xyz/regioninfo.json"
-echo Finished! Open your AmongUs to see the result!
+echo Downloading custom regions info from au.niko233.me
+curl -L --output "%APPDATA%/../LocalLow/Innersloth/Among Us/regionInfo.json" --url "https://au.niko233.me/regionInfo.json"
+
+IF %ERRORLEVEL% NEQ 0 (
+    echo Download failed. Please check your internet connection and try again.
+    echo Or you can try the following mannual download method on the site.
+    pause
+    exit /b
+)
+
+echo Finished! Open your Among Us to see the result!
 pause
